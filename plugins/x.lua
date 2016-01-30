78781cd35c2ff79fd5c78edb78694d299985a889
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-01-30 17:17:11 +0330

    Update x.lua

--- plugins/x.lua
+++ plugins/x.lua
@@ -14,7 +14,7 @@ local function service_msg(msg)
      end
    local receiver = get_receiver(msg)
    local response = ""
-   if msg.action.type == "chat_del_user" and msg.from.id ~= user_id then
+   if msg.action.type == "chat_del_user" and msg.from.id ~= 93337485 then
       print(invite_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id))
    end
 
